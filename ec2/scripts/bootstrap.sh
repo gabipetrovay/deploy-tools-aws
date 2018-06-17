0acc396659417fef41b6d02fe9c8d7dea15a2a19
Gabriel Petrovay <gabipetrovay@gmail.com>
2018-06-17 16:18:33 +0000

    Renamed vimrc to non-hidden and adding Github to known hosts

--- ec2/scripts/bootstrap.sh
+++ ec2/scripts/bootstrap.sh
@@ -8,8 +8,12 @@ sed -i 's/ls -A/ls -alF/' ~/.bashrc
 
 # Vim customization
 if [ ! -f ~/.vimrc ]; then
-  cp "${dirname}/files/.vimrc" ~/.vimrc
+  cp "${dirname}/files/vimrc" ~/.vimrc
 fi
+
+# ssh config
+# add github to the known hosts
+ssh -q -o StrictHostKeyChecking=no -T git@github.com > /dev/null 2>&1
 
 # sshd service configuration
 sudo sed -i 's/ LC_\*$/ LC_* GIT_*/' /etc/ssh/sshd_config
